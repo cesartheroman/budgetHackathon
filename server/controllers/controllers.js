07478860bec4ae7be8244aa044a271b230124e64
Cesar Roman <cesardroman18@gmail.com>
2020-06-18 11:57:23 -0400

    testing new update

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -3,7 +3,7 @@ const { Transaction } = require('../models/models.js');
 module.exports = {
   getAll: (req, res) => {
     //Transaction.find().then().catch()
-    // res.sendStatus(200);
+    res.sendStatus(200);
   },
 
   postAll: (req, res) => {
