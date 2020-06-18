452f16773ad65b3811069a7551b400c14169a502
Shannon Connolly <shannonconnolly92@gmail.com>
2020-06-18 17:10:10 -0400

    Render TransactionInput component to page and add basic styling

--- client/components/TransactionInput.jsx
+++ client/components/TransactionInput.jsx
@@ -1,33 +1,62 @@
 import React from 'react';
+import axios from 'axios';
 
 class TransactionInput extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            date: null, 
+            date: '', 
             description: '',
-            amount: null, 
-            category: null,
-            accountName: null
+            amount: 0, 
+            category: '',
+            accountName: ''
         };
         this.handleClick = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleClick() {
-
+        axios.post('/', this.state)
+          .then(() => {})
+          .catch((err) => console.log(err));
     }
 
     handleChange(e) {
-
+        let transactionObj = {};
+        transactionObj[e.target.name] = e.target.value;
+        this.setState(transactionObj);
     }
 
     render() {
         return (
-            <div>
-            
-            </div>
+           <form>
+                <label>Date:  
+                    <input type="text" name="date" value={this.state.date} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>Description:  
+                    <input type="text" name="description" value={this.state.description} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>Amount:  
+                    <input type="text" name="amount" value={this.state.amount} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>Category:  
+                    {/* <Categories value={this.state.category} onChange={this.handleChange} /> */}
+                </label>
+                <br/>
+                <label>Account Name:  
+                    <select name="accountName" onChange={this.handleChange}>
+                        <option value="debit">debit</option>
+                        <option value="credit">credit</option>
+                    </select>
+                </label>
+                <br/>
+                <input type="submit" value="Submit"/>
+                <br/>
+           </form>
         );
     }
 };
